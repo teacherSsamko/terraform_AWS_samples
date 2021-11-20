d7639b6a02638ecdffd012f79595e368454f4c5d
teacherSsamko <ssamko@kakao.com>
2021-11-20 13:37:49 +0900

    webserver: outputs: add public_ip of instance

--- webserver/outputs.tf
+++ webserver/outputs.tf
@@ -1,3 +1,8 @@
 output "ec2_id" {
   value = aws_instance.app_server.id
 }
+
+output "public_ip" {
+  value       = aws_instance.app_server.public_ip
+  description = "Public IP address of the web server"
+}
