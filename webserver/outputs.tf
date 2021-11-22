707c0f6c7539289bc703318124bd25abcd437f99
ssamko <teacher.ssamko@gmail.com>
2021-11-22 22:59:08 +0900

    webserver: outputs: add dns name

--- webserver/outputs.tf
+++ webserver/outputs.tf
@@ -1,8 +1,4 @@
-output "ec2_id" {
-  value = aws_instance.app_server.id
-}
-
-output "public_ip" {
-  value       = aws_instance.app_server.public_ip
-  description = "Public IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "DNS name of the load balancer"
 }
