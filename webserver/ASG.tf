9171a23883741eebf20c359912fa0f32e065c18a
ssamko <teacher.ssamko@gmail.com>
2021-11-22 22:58:48 +0900

    webserver: ASG: add target group arns

--- webserver/ASG.tf
+++ webserver/ASG.tf
@@ -18,6 +18,8 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
 
   min_size = 2
   max_size = 10
